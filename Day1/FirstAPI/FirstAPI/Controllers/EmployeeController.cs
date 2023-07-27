df15694830d98d60ae12abd14e935b1f797ff4b7
Gayathri Mahadevan <gayat_19@yahoo.com>
2023-07-27 08:23:40 +0530

    Day1

---
+++ Day1/FirstAPI/FirstAPI/Controllers/EmployeeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        static List<string> names = new List<string>{ "Sita", "Rita", "Bita", "Kita" };
+        [HttpGet]
+        public string[] Get()
+        {
+            return names.ToArray();
+        }
+        [HttpPost]
+        public void Create(string name)
+        {
+            names.Add(name);
+        }
+    }
+}
